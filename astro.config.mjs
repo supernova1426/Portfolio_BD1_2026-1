e8157e9ad7077cdbd80aa9f9df0156268467c428
supernova1426 <danny.ttph14@gmail.com>
2026-02-14 20:16:52 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+   site: 'https://danielaSerrato.github.io',
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
